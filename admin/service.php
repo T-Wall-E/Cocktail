ed5339d9499cb053d90cd75568c0884999a5c788
Tobias Wallewein <tobias.wallewein@gmail.com>
2014-01-27 23:59:56 +0100

    * prepare for service functions in control panel
    * Es muss nur noch der eigentl. shell_execute eingetragen werden

--- admin/service.php
+++ admin/service.php
@@ -1,4 +1,26 @@
 <?php
 	echo "<h2>Cocktail-Service</h2>";
-	echo "Hier soll man später den Service (neu-)starten/stoppen";
+	
+	$form = "<form action='admin.php' method='POST'>";
+	$form .= "<input type='submit' name='start' value='Service starten'>";
+	$form .= "<input type='submit' name='stop' value='Service stoppen'>";
+	$form .= "<input type='submit' name='restart' value='Service neustarten'>";
+	$form .= "</form>";
+	
+	echo $form;
+	
+	if(isset($_POST['start']))
+	{
+		echo "TODO: Starten";
+	}
+	
+	if(isset($_POST['stop']))
+	{
+		echo "TODO: Stoppen";
+	}
+	
+	if(isset($_POST['restart']))
+	{
+		echo "TODO: Neustarten";
+	}
 ?>
