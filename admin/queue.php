bcf35569ab1d94ec983ac0a172528b873848fc1f
Tobias Wallewein <tobias.wallewein@gmail.com>
2014-01-30 21:49:08 +0100

    + Control-Pannel for the queue
    Konfiguration für die Warteschlange ermöglicht

--- admin/queue.php
+++ admin/queue.php
@@ -1,31 +1,73 @@
 <?php
-// Untested CODE!!!
+	echo "<h2>Cocktail-Warteschlange</h2>";
+	
+	echo "<p>Hier haben Sie die M&ouml;glichkeit alle Cocktails aus der Warteschlange zu l&ouml;schen. (Wenn welche vorhanden sind. ;) )</p>";
+	
+	
+	$queueDir = "queue" . DIRECTORY_SEPARATOR;
+	
+	if(isset($_POST['clear']))
+	{
+		$count = 0;
+		foreach (scandir($queueDir) as $item)
+		{
+			if ($item == '.' || $item == '..') continue;
+			
+			if(unlink($queueDir.$item))
+			{
+				$count++;
+			}
+			else
+			{
+				$_SESSION['error'] = "Fehler bei min. einer Datei!";
+			}
+		}
+		
+		if($count > 0)
+		{
+			$_SESSION['success'] = $count . " ";
+			if($count == 1)
+			{
+				$_SESSION['success'] .= "Datei wurde";
+			}
+			else
+			{
+				$_SESSION['success'] .= "Dateien wurden";
+			}
+			
+			$_SESSION['success'] .= " gel&ouml;scht.";
+			
+		}
+		// TODO: Ergebnis
 
-        echo "<h2>Cocktail-Warteschlange</h2>";
-        
-        $form = "<form action='admin.php' method='POST'>";
-        $form .= "<input type='submit' name='clear' value='Warteschlange leeren'>";
-        $form .= "</form>";
-        
-        echo $form;
-        
-        if(isset($_POST['clear']))
-        {
-              $dir = "..".DIRECTORY_SEPARATOR."queue";
-              $count = 0;
-              foreach (scandir($dir) as $item) {
-                  if ($item == '.' || $item == '..') continue;
-                  if(unlink($dir.DIRECTORY_SEPARATOR.$item))
-                  {
-                    $count++;
-                  }
-                  else
-                  {
-                    $_SESSION['error'] = "Fehler bei min. einer Datei!";
-                  }
-              }
-
-        }
-        
-        echo "Hier folgt eine Auflistung aller Dateien, die sich in der Warteschlange befinden...";
+	}
+	
+	$table = "<table border=\"1\">\n";
+	$table .= "<tr><th>#</th><th>Name</th></tr>";
+	$count = 0;
+	foreach(scandir($queueDir) as $file)
+	{
+		if ($file == '.' || $file == '..') continue;
+		$table .= "<tr>\n";
+		$table .= "<td>".++$count."</td>\n";
+		$table .= "<td>".$file."</td>\n";
+		$table .= "</tr>\n";
+	}
+	$table .= "</table>\n\n";
+	
+	
+	if($count > 0)
+	{
+		echo $table;
+	}
+	else
+	{
+		echo "<p>Aktuell sind keine Cocktails in der Warteschlange gelistet.</p>";
+	}
+	
+	$form = "<form action='admin.php' method='POST'>";
+	$form .= "<input type='submit' name='clear' value='Warteschlange leeren'>";
+	$form .= "</form>";
+	
+	echo $form;
 ?>
