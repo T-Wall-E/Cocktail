b090bc5b926a8b5260ed9bb0a5175b00ec0fe953
Tobias Wallewein <tobias.wallewein@gmail.com>
2014-03-05 00:16:42 +0100

    + Disclaimer for upload

--- admin/upload.php
+++ admin/upload.php
@@ -1,6 +1,16 @@
 <?php
 	include_once "functions.php";
+	include_once "dbCon.php";
 	
+	if (! isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+	{
+		$client_ip = $_SERVER['REMOTE_ADDR'];
+	}
+	else
+	{
+		$client_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+					
 	if(isset($_POST['fileSubmit']))
 	{
 		$_SESSION['success'] = "";
@@ -37,6 +47,22 @@
 				}
 				else
 				{
+					
+					$sqlImageIP = "INSERT INTO imageip "
+									. "(Dateiname, IP) "
+									. "VALUES "
+									. "('" . mysql_real_escape_string($uploadDir. $_FILES["file"]["name"]) . "', '" . $client_ip . "')";
+					
+					$resultSqlImageIP = mysql_query($sqlImageIP);
+					if($resultSqlImageIP)
+					{
+						$_SESSION['info'] .= "Ihre IP '" . $client_ip . "' wurde gespeichert.<br>";
+					}
+					else
+					{
+						$errors[]  = mysql_error();			
+					}
+					
 					move_uploaded_file($_FILES["file"]["tmp_name"], $uploadDir. $_FILES["file"]["name"]);
 					$_SESSION['success'] .=  "Stored in: " . $uploadDir . $_FILES["file"]["name"];
 				}
@@ -53,12 +79,35 @@
 	echo "<h2>Bilder-Upload</h2>";
 	
 	include "infopanel.php";
-
+	
 	$form = '<form action="admin.php" method="post" enctype="multipart/form-data">';
 	$form .= '<label for="file">Dateiname:</label>';
 	$form .= '<input type="file" name="file" id="file"><br>';
 	$form .= '<input type="submit" name="fileSubmit" value="Submit">';
 	$form .= '</form>';
-
+	
+	$rules = "<p>Bitte beachten Sie beim Upload folgende Regeln:</p>";
+	$rules .= "<ul>";
+	$rules .= "<li>";
+	$rules .= "Illegale, pornografische, Bilder die gegen das Deutsche Gesetz, oder Urheberrecht verstoßen sind nicht gestattet!";
+	$rules .= "</li>";
+	$rules .= "<li>";
+	$rules .= "Das hochgeladene Bild darf nicht die Privatsphäre und Rechte/Ehre/W&uuml;rde anderer verletzen!";
+	$rules .= "</li>";
+	$rules .= "<li>";
+	$rules .= "Der Seitenbetreiber haftet in keinem Fall für den Inhalt der hochgeladenen Bilder.";
+	$rules .= "</li>";
+	$rules .= "<li>";
+	$rules .= "Der Seitenbetreiber ist stets bemüht Bilder, die gegen die Regeln verstoßen schnellstmöglich zu entfernen.";
+	$rules .= "</li>";
+	$rules .= "<li>";
+	$rules .= "Als Missbrauch gemeldete Bilder werden umgehend vom Seitenbetreiber gelöscht";
+	$rules .= "</li>";
+	$rules .= "<li>";
+	$rules .= "Aus rechtlichen Gr&uuml;nden wird ihre IP-Adresse: '" . $client_ip . "' mitgespeichert. (§113a TKG)";
+	$rules .= "</li>";
+	$rules .= "</ul>";
+	
+	echo $rules;
 	echo $form;	
 ?>
