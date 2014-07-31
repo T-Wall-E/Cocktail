6cb71e3e19c45c96376273bd2a999af998871d6d
T-Wall-E <tobiaswallewein@gmail.com>
2014-07-31 10:15:24 +0200

    Update motor.php

--- admin/motor.php
+++ admin/motor.php
@@ -5,9 +5,51 @@ if(isset($_SESSION['GID']))
 	{
 		case 1:
 		case 2:
+		
+			$stepsequences = array("HalfStep", "SingleStep", "DoubleStep");
+		
+			include_once "functions.php";
+
+			if(isset($_POST['motor_update']))
+			{
+				// Werte in DB speichern
+				// TOOD:
+				echo "Stepsequence: " . $_POST['stepsequence'] . "<br>";
+				echo "Stepintervall: " . $_POST['stepintervall'] . "<br>";
+			}
+
 			echo "<h2>Motor</h2>";
-			echo "<p>Hier soll man Einstellungen zum Schrittmotor vornehmen k&ouml;nnen.</p>";
-	
+
+			include "infopanel.php";
+
+			
+			// Werte aus der Datenbank auslesen und selektierte anzeigen
+			$selStepSequence = 0;
+			
+			$description = "<p>Hier werden Einstellungen zum Schrittmotor vorgenommen. (Werden erst nach einem Neustart des Servuces wirksam)</p>";
+
+			$form = "<form action='admin.php' method='POST'>";
+			// StepSequence
+			$form .= "<p>Hier wird die Stepsequence festgelegt</br>";
+			$form .= "<select name='stepsequence' size='1'>";
+			for($i = 0; $i < count($stepsequences); $i++)
+			{
+				$form .= "<option";
+				$form .= $selStepSequence == $i ? " selected>" : "";
+				$form .= $stepsequences[$i] . "</option>";
+			}
+			$form .= "</select>";
+			// StepIntervall
+			$form .= "<p>Hier wird der Stepintervall festgelegt</br>";
+			$form .= "<input type='number' name='stepintervall' min='1' max='10'>";
+			
+			
+			$form .= "<input type='submit' name='motor_update' value='&Auml;nderungen &uuml;bernehmen'></p>";
+			$form .= "</form>";
+
+			echo $description;
+			echo $form;
+
 	break;
 	}
 }
